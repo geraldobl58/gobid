ea05e8431b5aed47fa7e0aa79af14c2dd9ce7fe1
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-27 15:42:02 -0300

    Implement user signup handler with JSON decoding and validation

--- internal/api/user_handler.go
+++ internal/api/user_handler.go
@@ -1,9 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/geraldobl58/gobid/internal/jsonutils"
+	"github.com/geraldobl58/gobid/internal/usecase/user"
+)
 
 func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented")
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+	}
 }
 
 func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
