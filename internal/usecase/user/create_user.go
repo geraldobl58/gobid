3bc0adfaaf4b4c9e14c37ae36327b42e2a1e3a50
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-27 12:49:56 -0300

    Add CreateUserReq struct with validation for user creation

---
+++ internal/usecase/user/create_user.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"context"
+
+	"github.com/geraldobl58/gobid/internal/validator"
+)
+
+type CreateUserReq struct {
+	UserName     string `json:"user_name"`
+	Email        string `json:"email"`
+	PasswordHash []byte `json:"password_hash"`
+	Bio          string `json:"bio"`
+}
+
+func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
+
+	var eval validator.Evaluator
+
+	eval.CheckField(validator.NotBlank(req.UserName), "user_name", "this field is required")
+
+	return eval
+}
