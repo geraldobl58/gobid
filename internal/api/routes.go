0cc8b2bd09c6eef0ec876cba4ef39a9cde85c67e
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-27 09:52:31 -0300

    Add route bindings for user authentication in API

---
+++ internal/api/routes.go
@@ -0,0 +1,16 @@
+package api
+
+import "github.com/go-chi/chi/v5"
+
+func (api *Api) BindRoues() {
+	api.Router.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			//example => /api/v1/health
+			r.Route("/users", func(r chi.Router) {
+				r.Post("/signup", api.handleSignUpUser)
+				r.Get("/login", api.handleLoginUser)
+				r.Get("/logout", api.handleLogoutUser)
+			})
+		})
+	})
+}
