46c242822cb9e5c71b3352f1df999f3524f193e1
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-29 10:54:51 -0300

    Refactor user service error handling and add user authentication method

--- internal/services/users_service.go
+++ internal/services/users_service.go
@@ -6,12 +6,16 @@ import (
 
 	"github.com/geraldobl58/gobid/internal/store/pgstore"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var ErrDuplicatedEmailOrPassoword = errors.New("email or username already exists")
+var (
+	ErrDuplicatedEmailOrUsername = errors.New("email or username already exists")
+	ErrInvalidCredentials        = errors.New("invalid credentials")
+)
 
 type UserService struct {
 	pool    *pgxpool.Pool
@@ -45,11 +49,35 @@ func (us *UserService) CreateUser(ctx context.Context, userName, email, password
 		var pgErr *pgconn.PgError
 
 		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			return uuid.UUID{}, ErrDuplicatedEmailOrPassoword
+			return uuid.UUID{}, ErrDuplicatedEmailOrUsername
 		}
 
 		return uuid.UUID{}, err
 	}
 
 	return id, nil
+}
+
+func (us *UserService) AuthenticatedUser(ctx context.Context, email, password string) (uuid.UUID, error) {
+	user, err := us.queries.GetUserByEmail(ctx, email)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return uuid.UUID{}, ErrInvalidCredentials
+		}
+
+		return uuid.UUID{}, err
+	}
+
+	err = bcrypt.CompareHashAndPassword(user.PasswordHash, []byte(password))
+
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return uuid.UUID{}, ErrInvalidCredentials
+		}
+
+		return uuid.UUID{}, err
+	}
+
+	return user.ID, nil
 }
