811ad3da00b4f0e79189c5817f8c4c23fb53d56c
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-29 10:54:41 -0300

    Enhance API routes with middleware and update login/logout methods to use POST

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,15 +1,20 @@
 package api
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func (api *Api) BindRoutes() {
+	api.Router.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger, api.Sessions.LoadAndSave)
+
 	api.Router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
 			//example => /api/v1/health
 			r.Route("/users", func(r chi.Router) {
 				r.Post("/signup", api.handleSignUpUser)
-				r.Get("/login", api.handleLoginUser)
-				r.Get("/logout", api.handleLogoutUser)
+				r.Post("/login", api.handleLoginUser)
+				r.With(api.AuthMiddleware).Post("/logout", api.handleLogoutUser)
 			})
 		})
 	})
